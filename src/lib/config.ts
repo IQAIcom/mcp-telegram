131c656d0957d08e7248cbffcfa6f6af355aca0c
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-07-28 12:11:53 +0530

    remove

--- src/lib/config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-	weatherApi: {
-		baseUrl: "https://api.openweathermap.org/data/2.5",
-		apiKey: process.env.OPENWEATHER_API_KEY || "",
-		defaultUnits: "metric", // metric (Celsius) or imperial (Fahrenheit)
-	},
-};
